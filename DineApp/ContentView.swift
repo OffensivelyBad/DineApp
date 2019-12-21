8bd1206e5dfa6f9f5e62c5f0a1a01411d3d43f2b
SRoller <sroller@justfab.com>
2019-12-21 15:44:42 -0500

    Initial Commit

---
+++ DineApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DineApp
+//
+//  Created by Shawn Roller on 12/21/19.
+//  Copyright © 2019 offensively-bad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
